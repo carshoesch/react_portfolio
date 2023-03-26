7d5c646715ba3ed358929aa70e8e413e46a22566
carshoesch <hcarsten1@googlemail.com>
2023-03-26 23:35:34 +0200

    changed max items for largest viewport to 4

--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -12,7 +12,7 @@ const Skills = () => {
         superLargeDesktop: {
             // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 3000 },
-            items: 5
+            items: 4
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
